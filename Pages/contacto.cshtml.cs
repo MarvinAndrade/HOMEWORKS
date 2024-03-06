8721d99514496ca2994b958d6492da630664956b
MarvinDev911 <mar.godtzy@gmail.com>
2024-03-06 15:54:56 -0600

    guia web

---
+++ Pages/contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace formularioContacto.Pages
+{
+    public class contactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
